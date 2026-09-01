e75a4a84ad36dac6387e377e3e3834e3d4373235
pavan <borusupavan.77.99@gmail.com>
2026-09-01 15:27:26 +0530

    Time: 16 ms (19.39%) | Memory: 35 MB (54.36%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -3,7 +3,6 @@ public:
     bool canship(vector<int>& weights, int dayshave, long long capacity) {
         int daysneeded = 1;
         long long currentweightsum = 0;
-
         for (int weight : weights) {
             if (currentweightsum + weight <= capacity) {
                 currentweightsum += weight;
@@ -12,28 +11,22 @@ public:
                 currentweightsum = weight;
             }
         }
-
         return daysneeded <= dayshave;
     }
-
     int shipWithinDays(vector<int>& weights, int days) {
         long long low = *max_element(weights.begin(), weights.end());
-
         long long high = 0;
         for (int w : weights) {
             high += w;
         }
-
         while (low < high) {
             long long mid = low + (high - low) / 2;
-
-            if (canship(weights, days, mid)) {
+           if (canship(weights, days, mid)) {
                 high = mid;
             } else {
                 low = mid + 1;
             }
         }
-
         return low;
     }
 };
